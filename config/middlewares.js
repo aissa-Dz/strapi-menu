234012d573288bb8bfd7697c52ddfd13cb058890
aissa-Dz <38842233+aissa-Dz@users.noreply.github.com>
2024-10-04 14:14:02 +0100

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,64 @@
+
+// config/middlewares.js
+
 module.exports = [
-  'strapi::logger',
+
   'strapi::errors',
-  'strapi::security',
+  
+  {
+  
+  name: 'strapi::security',
+  
+  config: {
+  
+  contentSecurityPolicy: {
+  
+  useDefaults: true,
+  
+  directives: {
+  
+  'connect-src': ["'self'", 'https:'],
+  
+  'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+  
+  'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+  
+  upgradeInsecureRequests: null,
+  
+  },
+  
+  },
+  
+  cors: {
+  
+  origin: ['http://localhost:1337', 'https://preview--control-panel-dashboard-49.gptengineer.run'], // أضف هنا عناوين URL المسموح بها
+  
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  
+  headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  
+  keepHeaderOnError: true,
+  
+  },
+  
+  },
+  
+  },
+  
   'strapi::cors',
+  
   'strapi::poweredBy',
+  
+  'strapi::logger',
+  
   'strapi::query',
+  
   'strapi::body',
+  
   'strapi::session',
+  
   'strapi::favicon',
+  
   'strapi::public',
-];
+  
+  ];
